7f457373f3a7675f5942ba57550e8845da3ec142
ayushy78 <ayushy78@gmail.com>
2022-05-30 01:52:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import './App.css';
 import PreNavbar from './components/PreNavbar';
 import Navbar from "./components/Navbar.js"
-import {BrowserRouter as Router} from 'react-router-dom'
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import Slider from "./components/Slider.js"
 import data from "./data/data.json"
-
+import Offers from "./components/Offers.js"
+import Offer from "./components/Offer.js"
+import Heading from "./components/Heading.js"
+import StarProduct from "./components/StarProduct.js"
+import HotAccessoriesMenu from "./components/HotAccessoriesMenu.js"
+import HotAccessories from "./components/HotAccessories.js"
 function App() {
   return (
       <Router>
           <PreNavbar/>
           <Navbar/> 
           <Slider start={data.banner.start}/>
-      </Router>
+          <Offers offer= {data.offer}> </Offers>
+          <Heading text={"Star Products"}></Heading>
+          <StarProduct starProduct={data.starProduct}/>
+          <Heading text="HOT ACCESSORIES" />
+          <HotAccessoriesMenu />
+          <Route exact path="/music">
+       <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />
+       </Route>
+      </Router>  
   );
 }
 
